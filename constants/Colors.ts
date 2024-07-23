b3c3102deb1de6d81ecab9eb82084648b9307640
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-23 19:01:09 -0300

    Constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,6 +5,7 @@
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
+const tintColor = 'white';
 
 export const Colors = {
   light: {
@@ -23,4 +24,35 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  blue: '#5D96FB',
+  filterCategory: '#F5197B',
+  greenTrivia: '#48DA97',
+  gray: '#F2F2F2',
+  gray1: '#C1C1C1',
+  grayChat: '#B3B3B3',
+  whiteBackGround: '#FFFFFF',
+  grayText1: '#585858',
+  grayText2: '#666666',
+  pink: '#FFB4B4',
+  primary: '#009FE3',
+  primary_variant: '#E73843',
+  secondary: '#1D345E',
+  navigator: '#000000',
+  text: 'white',
+  titleGray: '#575757',
+  grayInput: '#f4f4f4',
+  discord: '#5865F2',
+  facebook: '#3b5998',
+  nonFocused: 'white',
+  googlePrimary: '#346CF1',
 };
