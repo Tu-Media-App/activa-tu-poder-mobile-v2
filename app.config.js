5112cc7535689cbcbf754495d9616aebf6350e6a
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-23 16:29:50 -0300

    update config

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-{
+export default {
   "expo": {
     "name": "activa-tu-poder-mobile-v2",
     "slug": "activa-tu-poder-mobile-v2",
@@ -13,9 +13,13 @@
       "backgroundColor": "#ffffff"
     },
     "ios": {
-      "supportsTablet": true
+      "googleServicesFile": process.env.GOOGLE_SERVICES_INFOPLIST,
+      "bundleIdentifier": "com.activatupoder",
+      "supportsTablet": true,
     },
     "android": {
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON,
+      "package": "com.activatupoder",
       "adaptiveIcon": {
         "foregroundImage": "./assets/images/adaptive-icon.png",
         "backgroundColor": "#ffffff"
@@ -27,10 +31,28 @@
       "favicon": "./assets/images/favicon.png"
     },
     "plugins": [
-      "expo-router"
+      "expo-router",
+      "@react-native-google-signin/google-signin",
+      "@react-native-firebase/app",
+      [
+        "expo-build-properties",
+        {
+          "ios": {
+            "useFrameworks": "static"
+          }
+        }
+      ]
     ],
     "experiments": {
       "typedRoutes": true
+    },
+    "extra": {
+      "router": {
+        "origin": false
+      },
+      "eas": {
+        "projectId": "98ab56b0-9776-4f74-af69-f4440b0e930f"
+      }
     }
   }
 }
