e1888560c6fe8baa6c64309c82bb91708fcc1e73
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-26 18:17:25 -0300

    update config

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,7 @@ type Config = {
   firebaseClientId: string;
   redirectUri: string;
   googleWebClientId: string;
+  termsUrl: string;
   
 };
 
@@ -9,4 +10,5 @@ export const config: Config = {
   firebaseClientId: process.env.CLIENT_ID || 'secret',
   redirectUri: process.env.REDIRECT_URI || 'http://localhost:3000',
   googleWebClientId: process.env.GOOGLE_WEB_CLIENT_ID || 'secret',
+  termsUrl: 'https://luisapp.vercel.app/terms.html',
 };
