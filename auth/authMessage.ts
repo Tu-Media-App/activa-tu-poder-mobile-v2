ed2be86839b24c7b9b1718f6c8b604384e0bc987
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-28 17:35:26 -0300

    auth constant and types

---
+++ auth/authMessage.ts
@@ -0,0 +1,10 @@
+export const AuthMessage: { [code: string]: string } = {
+  'auth/invalid-email': 'Correo no válido',
+  'auth/user-disabled': 'Usuario deshabilitado',
+  'auth/user-not-found': 'Usuario no registrado',
+  'auth/wrong-password': 'Credenciales incorrectas',
+  'auth/too-many-requests': 'Demasiados intentos, intente más tarde',
+  'auth/email-already-in-use': 'Usuario ya registrado',
+  'auth/operation-not-allowed': 'Su cuenta no tiene permiso para acceder, por favor contactese con un administrador',
+  'auth/weak-password': 'La contraseña es muy sencilla',
+};
