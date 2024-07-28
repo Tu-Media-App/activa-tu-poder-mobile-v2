3a13b36b760322dc5765d7a5cde8feb55f1001a8
Hugo Larrea <hlarrea@fi.uba.ar>
2024-07-28 17:34:50 -0300

    complete profile hook

---
+++ hooks/firebase/useCompleteProfile.ts
@@ -0,0 +1,29 @@
+import { ProfileSheetFields } from '@/entities/users/profileSheetFields';
+import firestore from '@react-native-firebase/firestore';
+
+export const useCompleteProfile = () => {
+  const completeProfile = async (fields: ProfileSheetFields, uid: string) => {
+    const ref = firestore().doc(`users/${uid}`);
+    const { gender, country, province, city, birthdate } = fields;
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const user: any = {
+        updatedAt: firestore.Timestamp.now(),
+        birthdate,
+        gender,
+        country,
+      };
+
+      if (province) user.province = province;
+      if (city) user.city = city;
+
+      await ref.set(user, { merge: true });
+      return true;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { completeProfile };
+};
